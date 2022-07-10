cafd6ccccca32c87ae947f062b5c61eccab189b9
unknown <mudits02@gmail.com>
2022-07-10 17:10:38 +0530

    Initialized TFOD API

---
+++ Train_Test_EDA.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import os
+import shutil
+import glob as glob
+
+
+def split_data(path_to_data, path_to_save_train, path_to_save_valid, split_size=0.1):
+
+    folders = os.listdir(path_to_data)
+
+    for folder in folders:
+
+        full_path = os.path.join(path_to_data, folder)
+        # importing only .png files(images)
+        images_path = glob.glob(os.path.join(full_path, '*.png'))
+
+        x_train, x_val = train_test_split(images_path, test_size=split_size)
+
+        for x in x_train:
+            ##features = tf.layers.batch_normalization(##)
+            path_to_folder = os.path.join(path_to_save_train, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
+
+        for x in x_val:
+            path_to_folder = os.path.join(path_to_save_valid, folder)
+
+            if not os.path.isdir(path_to_folder):
+                os.makedirs(path_to_folder)
+
+            shutil.copy(x, path_to_folder)
